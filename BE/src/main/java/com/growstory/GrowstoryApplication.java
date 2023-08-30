c6aeb4ddb61d9f5519bfab97e1bf658aca243e29
DoKyung-Hwang <dokyung226@gmail.com>
2023-08-30 14:08:11 +0900

    🔧 Conf : .gitignore setting

---
+++ BE/src/main/java/com/growstory/GrowstoryApplication.java
@@ -0,0 +1,13 @@
+package com.growstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowstoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowstoryApplication.class, args);
+	}
+
+}
