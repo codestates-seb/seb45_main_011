5a18ebcdf37cbe3087b07c34134a71ed0092ae15
hanbinchoi <wkghskak@naver.com>
2023-09-01 14:10:55 +0900

    [FE] ♻️ Intro 컴포넌트 리팩토링

--- client/src/components/common/Intro.tsx
+++ client/src/components/common/Intro.tsx
@@ -8,47 +8,44 @@ interface IntroProps extends DefaultProps {
 
 export default function Intro({ children }: IntroProps) {
   return (
-    <div className="flex justify-center items-center p-[8px] max-w-[628px]  bg-gradient-to-b from-[#EC9355] to-[#DC7737] rounded-xl">
-      <div className="relative p-[52px] flex flex-col gap-[28px] items-center w-full h-full bg-[url('/assets/img/bg_wood_yellow.svg')] rounded-md">
-        <Image
-          className="absolute top-2 left-2"
-          src="assets/img/screw.svg"
-          alt=""
-          width={16}
-          height={16}
-        />
-        <Image
-          className="absolute top-2 right-2"
-          src="assets/img/screw.svg"
-          alt=""
-          width={16}
-          height={16}
-        />
-        <Image
-          className="absolute bottom-2 left-2"
-          src="assets/img/screw.svg"
-          alt=""
-          width={16}
-          height={16}
-        />
-        <Image
-          className="absolute bottom-2 right-2"
-          src="assets/img/screw.svg"
-          alt=""
-          width={16}
-          height={16}
-        />
-        <Logo size="large" />
-        <div className="flex flex-col gap-[12px] text-center">
-          {children}
-          {/*
-          children:  
-          <p className="text-[#A54426] text-2xl font-bold">키우는 식물로</p>
-            <p className="text-[#A54426] text-2xl font-bold">
-              <b className="text-[#4D2113] text-[2rem]">나만의 정원</b>을 꾸며 보세요!
-          </p>
-         */}
-        </div>
+    <div className="relative flex flex-col gap-[28px] justify-center items-center max-w-[626px] h-[392px] rounded-md border-gradient ">
+      <Image
+        className="absolute top-2 left-2"
+        src="assets/img/screw.svg"
+        alt=""
+        width={16}
+        height={16}
+      />
+      <Image
+        className="absolute top-2 right-2"
+        src="assets/img/screw.svg"
+        alt=""
+        width={16}
+        height={16}
+      />
+      <Image
+        className="absolute bottom-2 left-2"
+        src="assets/img/screw.svg"
+        alt=""
+        width={16}
+        height={16}
+      />
+      <Image
+        className="absolute bottom-2 right-2"
+        src="assets/img/screw.svg"
+        alt=""
+        width={16}
+        height={16}
+      />
+      <Logo size="large" />
+      <div className="flex flex-col gap-[12px] text-center">
+        {children}
+        {/* children:
+        <p className="text-[#A54426] text-2xl font-bold">{INTRO_DESC.first}</p>
+        <p className="text-[#A54426] text-2xl font-bold">
+          <b className="text-[#4D2113] text-[2rem]">{INTRO_DESC.bold}</b>
+          {INTRO_DESC.second}
+        </p> */}
       </div>
     </div>
   );
--- client/src/constants/contents.ts
+++ client/src/constants/contents.ts
@@ -2,3 +2,9 @@ export const CONTROL_BUTTON_TITLE = {
   edit: '수정하기',
   delete: '삭제하기',
 } as const;
+
+export const INTRO_DESC = {
+  first: '키우는 식물로',
+  second: '을 꾸며 보세요!',
+  bold: '나만의 정원',
+} as const;
