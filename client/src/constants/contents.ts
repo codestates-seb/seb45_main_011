dfcfb39a64c8d03d7ce9e80a4e8b09d20a48a1dc
nalsae <nalsae0207@naver.com>
2023-09-01 15:30:50 +0900

    [FE] ✨ Feat : MapController 컴포넌트의 상수 추가 (#40)

--- client/src/constants/contents.ts
+++ client/src/constants/contents.ts
@@ -1,4 +1,4 @@
-export const CONTROL_BUTTON_TITLE = {
+export const CONTROL_BUTTON_TITLES = {
   edit: '수정하기',
   delete: '삭제하기',
 } as const;
@@ -15,4 +15,11 @@ export const INTRO_DESC = {
   first: '키우는 식물로',
   second: '을 꾸며 보세요!',
   bold: '나만의 정원',
+} as const;
+
+export const CONTROLLER_TITLES = {
+  up: '위쪽',
+  right: '오른쪽',
+  down: '아래쪽',
+  left: '왼쪽',
 } as const;
---
+++ client/src/constants/values.ts
@@ -0,0 +1,1 @@
+export const CONTROLLER_DIRECTIONS = ['up', 'right', 'down', 'left'] as const;
