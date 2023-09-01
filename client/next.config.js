ea1962b25904dfb858ac84e550fd0dcdc6f34fee
nalsae <nalsae0207@naver.com>
2023-09-01 17:30:48 +0900

    [FE] 🔧 Conf : 외부 이미지 관련 Next.js 환경 설정 추가 (#41)

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'growstory.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/image/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
