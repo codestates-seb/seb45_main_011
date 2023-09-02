165e10080b374a1ee8b21f34b32605bebc249a5c
심도연 <shimdokite@gmail.com>
2023-09-02 16:26:42 +0900

    [FE] ✨ Feat: ConnectCheckBox 컴포넌트 구현 (#73)

--- client/src/app/board/page.tsx
+++ client/src/app/board/page.tsx
@@ -1,3 +1,10 @@
+import ConnectCheckBox from '@/components/common/ConnectCheckBox';
+
 export default function Board() {
-  return <div>게시판 페이지입니다!</div>;
+  return (
+    <div>
+      게시판 페이지입니다!
+      <ConnectCheckBox isChecked={true} />
+    </div>
+  );
 }
