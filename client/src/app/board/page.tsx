0921714c7845a779ba168db27233a1ce2de0eb80
심도연 <shimdokite@gmail.com>
2023-09-02 23:22:26 +0900

    [FE] connectCheckBox 컴포넌트 임시 테스트 코드 삭제

--- client/src/app/board/page.tsx
+++ client/src/app/board/page.tsx
@@ -1,10 +1,3 @@
-import ConnectCheckBox from '@/components/common/ConnectCheckBox';
-
 export default function Board() {
-  return (
-    <div>
-      게시판 페이지입니다!
-      <ConnectCheckBox isChecked={true} />
-    </div>
-  );
+  return <div>게시판 페이지입니다!</div>;
 }
