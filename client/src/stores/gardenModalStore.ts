6309d746788ca9a8d9a264d4c344e5b3f6e68998
nalsae <nalsae0207@naver.com>
2023-09-06 01:55:16 +0900

    [FE] ♻️ Refactor : gardenModalStore 리팩토링 (#102)

---
+++ client/src/stores/gardenModalStore.ts
@@ -0,0 +1,28 @@
+import { create } from 'zustand';
+
+export type GardenModalType =
+  | 'leafExist'
+  | 'noLeafExist'
+  | 'selectLeaf'
+  | 'purchaseInfo'
+  | 'purchase'
+  | 'emptyInventory'
+  | null;
+
+interface GardenModalState {
+  isOpen: boolean;
+  type: GardenModalType;
+  changeType: (type: GardenModalType) => void;
+  open: () => void;
+  close: () => void;
+}
+
+const useGardenModalStore = create<GardenModalState>((set) => ({
+  isOpen: false,
+  type: null,
+  changeType: (type) => set(() => ({ type })),
+  open: () => set(() => ({ isOpen: true })),
+  close: () => set(() => ({ isOpen: false })),
+}));
+
+export default useGardenModalStore;
