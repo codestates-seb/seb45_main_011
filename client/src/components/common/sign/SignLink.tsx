e68bc9c801e2abbd874cdbe0d49b247871e14eff
심도연 <shimdokite@gmail.com>
2023-09-07 15:54:47 +0900

    [FE] ✨ Feat: SignLink 컴포넌트 구현

--- client/src/components/common/sign/SignLink.tsx
+++ client/src/components/common/sign/SignLink.tsx
@@ -1,15 +1,23 @@
 import Link from 'next/link';
 import { SIGN_LINK_TO } from '@/constants/contents';
+import { DefaultProps } from '@/types/common';
 
-interface SignLinkProps {
+interface SignLinkProps extends DefaultProps {
   type: 'signin' | 'signup';
   route: '/signin' | '/signup';
   text: 'signinText' | 'signupText';
+  onLinkTo?: () => void;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function SignLink({ type, route, text }: SignLinkProps) {
+export default function SignLink({
+  type,
+  route,
+  text,
+  onLinkTo,
+}: SignLinkProps) {
   return (
-    <div className="text-brown-90">
+    <div className="text-brown-90" onClick={onLinkTo}>
       <Link href={route}>
         {SIGN_LINK_TO[text]}
         <span className="font-bold">&nbsp;{SIGN_LINK_TO[type]}</span>
