8f100552aee6a28cebe50aff34bf8f56637ce24f
nalsae <nalsae0207@naver.com>
2023-09-08 01:22:09 +0900

    [FE] ✨ Feat : leafsStore 임시 생성 (#101)

---
+++ client/src/stores/leafsStore.ts
@@ -0,0 +1,22 @@
+import create from 'zustand';
+
+interface LeafsState {
+  isModalOpen: boolean;
+  deleteTargetLeafsId: number | null;
+
+  modalOpen: () => void;
+  modalClose: () => void;
+  setDeleteTargetId: (deleteTargetId: number) => void;
+}
+
+const useLeafsStore = create<LeafsState>((set) => ({
+  isModalOpen: false,
+  deleteTargetLeafsId: null,
+
+  modalOpen: () => set((state) => ({ ...state, isModalOpen: true })),
+  modalClose: () => set((state) => ({ ...state, isModalOpen: false })),
+  setDeleteTargetId: (deleteTargetLeafsId) =>
+    set((state) => ({ ...state, deleteTargetLeafsId })),
+}));
+
+export default useLeafsStore;
