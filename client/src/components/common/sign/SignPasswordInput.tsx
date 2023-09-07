1a84f342e058c6dbe80b33ebe6b12c0dfe227c04
심도연 <shimdokite@gmail.com>
2023-09-07 15:55:36 +0900

    [FE] ✨ Feat: SignPasswordInput 컴포넌트 구현

--- client/src/components/common/sign/SignPasswordInput.tsx
+++ client/src/components/common/sign/SignPasswordInput.tsx
@@ -1,6 +1,11 @@
 'use client';
 
-import { UseFormRegister, FieldErrors, UseFormWatch } from 'react-hook-form';
+import {
+  UseFormRegister,
+  FieldErrors,
+  UseFormWatch,
+  RegisterOptions,
+} from 'react-hook-form';
 
 import { SIGNIN_REQUIRE, SIGNIN_VAILDATION } from '@/constants/contents';
 import { SignFormValue } from '@/types/common';
@@ -9,7 +14,7 @@ interface SignPasswordInputProps {
   tag: 'password' | 'passwordCheck';
   register: UseFormRegister<SignFormValue>;
   errors: FieldErrors<SignFormValue>;
-  watch?: UseFormWatch<SignFormValue>;
+  watch: UseFormWatch<SignFormValue>;
   disabled?: boolean;
 }
 
@@ -28,19 +33,23 @@ export default function SignPasswordInput({
   ) => {
     if (tag === 'password') {
       return {
-        required: true,
-        pattern: {
-          value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,12}$/,
-          message: SIGNIN_VAILDATION[tag],
+        validation: {
+          required: true,
+          minLenght: {
+            value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/,
+            message: SIGNIN_VAILDATION[tag],
+          },
         },
       };
     }
 
     if (tag === 'passwordCheck' && watch) {
       return {
-        required: true,
-        validate: (value: string) =>
-          value === watch('password') || '비밀번호가 일치하지 않습니다.',
+        validation: {
+          required: true,
+          validate: (value: string) =>
+            value === watch('password') || SIGNIN_VAILDATION[tag],
+        },
       };
     }
 
@@ -49,15 +58,15 @@ export default function SignPasswordInput({
 
   const registerFormat = getRegisterByType(tag, watch);
 
-  // if (registerFormat === null) return null;
-
   return (
     <div className="flex flex-col">
       <input
         type="password"
         className={`min-w-[300px] pl-9 py-[10px] font-normal text-[12px] border-2 border-brown-70 rounded-lg bg-[center_left_12px] bg-no-repeat ${SIGN_INPUT_BG[tag]} leading-[12px] outline-none shadow-outer/down`}
         placeholder={SIGNIN_REQUIRE[tag]}
-        {...register(tag, registerFormat)}
+        // html required 사용 권장!
+        required
+        {...register(tag, registerFormat?.validation)}
         disabled={disabled}
       />
       {errors[tag] && (
