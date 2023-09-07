507bdbe150e67f1feaa950626556a24a779a043a
nalsae <nalsae0207@naver.com>
2023-09-08 01:35:03 +0900

    [FE] ✨ Feat : Leaf 컴포넌트에 prop 추가 (#101)
    - Garden 페이지에서의 식물 카드를 선택하는 로직 관련

--- client/src/components/common/Leaf.tsx
+++ client/src/components/common/Leaf.tsx
@@ -1,63 +1,102 @@
 'use client';
 
 import Image from 'next/image';
-import { useState } from 'react';
-
-import { DefaultProps, LeafInfo } from '@/types/common';
+import { useRouter } from 'next/navigation';
 
 import ControlButton from './ControlButton';
 import LeafName from './LeafName';
 
-interface LeafProps extends DefaultProps {
+import useLeafsStore from '@/stores/leafsStore';
+
+interface LeafProps {
   location: 'garden' | 'leaf';
-  data: LeafInfo;
+  userId?: number;
+  imageUrl: string;
+  name: string;
+  leafId: number;
+  selectedLeafId?: number | null;
+  onClick?: (event: React.MouseEvent<HTMLDivElement>) => void;
 }
 
-export default function Leaf({ location, data }: LeafProps) {
-  const [isClicked, setIsClicked] = useState(false);
-  const handleLeafClick = () => {
-    if (location === 'leaf') {
-      console.log('식물 카드 목록에서 클릭된 Leaf');
+export default function Leaf({
+  location,
+  name,
+  imageUrl,
+  userId,
+  leafId,
+  selectedLeafId,
+  onClick,
+}: LeafProps) {
+  const router = useRouter();
+
+  const modalOpen = useLeafsStore((state) => state.modalOpen);
+  const setDeleteTargetId = useLeafsStore((state) => state.setDeleteTargetId);
+
+  const handleLeafClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (onClick) {
+      onClick(event);
       return null;
     }
 
-    console.log('정원 페이지에서 클릭된 Leaf');
-    setIsClicked((previous) => !previous);
+    if (location === 'leaf') {
+      router.push(`/leaf/${userId}/${leafId}`);
+      return null;
+    }
   };
+
+  const handleEdit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    router.push(`/leaf/edit/${userId}/${leafId}`);
+  };
+
+  const handleDelete = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    leafId: number,
+  ) => {
+    e.stopPropagation();
+    setDeleteTargetId(leafId);
+    modalOpen();
+  };
+
   return (
     <div
       className={
-        'relative flex flex-col items-center w-[200px] gap-2 cursor-pointer bg-transparent'
+        'relative flex flex-col items-center w-[200px] h-[205px] gap-2 cursor-pointer bg-transparent'
       }
       role="button"
+      data-leaf-id={leafId}
       onClick={handleLeafClick}>
-      {location === 'garden' && isClicked ? (
+      {location === 'garden' && selectedLeafId === leafId ? (
         <div className="absolute flex justify-center items-center w-full h-full border-4 border-dashed border-brown-70 rounded-lg shadow-outer/down bg-brown-20 bg-opacity-[76%]">
           <Image
             src={'/assets/img/checked.svg'}
             alt="로고"
             width={36}
             height={28}
+            style={{ width: 'auto', height: 'auto' }}
           />
         </div>
       ) : null}
 
-      {location === 'leaf' ? (
-        <div className="flex gap-2 absolute right-2.5 top-2.5">
-          <ControlButton usage="edit" />
-          <ControlButton usage="delete" />
+      {location === 'leaf' && (
+        <div className="flex h-full gap-2 absolute right-2.5 top-2.5">
+          <ControlButton usage="edit" handleEdit={handleEdit} />
+          <ControlButton
+            usage="delete"
+            handleDelete={(event) => handleDelete(event, leafId)}
+          />
         </div>
-      ) : null}
+      )}
 
       <Image
-        src={data.imageUrl}
-        alt="로고"
+        src={imageUrl || ''}
+        alt={name}
         width={200}
         height={160}
         className="object-cover w-[200px] h-[160px] rounded-xl border-2 border-brown-50 shadow-outer/down"
       />
 
-      <LeafName name={data.leafNickname} />
+      <LeafName name={name} />
     </div>
   );
 }
