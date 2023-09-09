fab17f5ad6233a9e6029f4f7ed8d47b617c77d8b
hanbinchoi <wkghskak@naver.com>
2023-09-09 21:22:26 +0900

    [FE] ✨ EmptyDiary 분기 처리

--- client/src/components/Leaf/EmptyDiary.tsx
+++ client/src/components/Leaf/EmptyDiary.tsx
@@ -14,8 +14,6 @@ export default function EmptyDiary({ pathUserId, userId }: EmptyDiaryProps) {
     setModalCategory('add');
   };
 
-  if (pathUserId !== userId) return null;
-
   return (
     <div className="w-full flex flex-col items-center">
       <div className="w-full pt-6 pb-5 flex flex-col gap-[1.1rem] justify-center items-center max-w-[414px] h-[137px] bg-brown-10 border-2 border-brown-50 rounded-lg">
@@ -23,15 +21,19 @@ export default function EmptyDiary({ pathUserId, userId }: EmptyDiaryProps) {
           <p className="font-bold text-[1.25rem] text-brown-70">
             등록된 일지가 없어요 : (
           </p>
-          <p className=" font-bold text-[1rem] text-brown-50">
-            일지를 작성해보세요!
-          </p>
+          {pathUserId === userId && (
+            <p className=" font-bold text-[1rem] text-brown-50">
+              일지를 작성해보세요!
+            </p>
+          )}
         </div>
-        <button
-          className="px-3 py-2 bg-[url('/assets/img/bg_wood_dark.png')] bg-contain border-2 border-brown-70 rounded-lg shadow-outer/down text-sm font-bold text-brown-10"
-          onClick={addDiary}>
-          일지 작성
-        </button>
+        {pathUserId === userId && (
+          <button
+            className="px-3 py-2 bg-[url('/assets/img/bg_wood_dark.png')] bg-contain border-2 border-brown-70 rounded-lg shadow-outer/down text-sm font-bold text-brown-10"
+            onClick={addDiary}>
+            일지 작성
+          </button>
+        )}
       </div>
       {/* 질문:
         1. 아래 코드가 동작 안하는 이유
