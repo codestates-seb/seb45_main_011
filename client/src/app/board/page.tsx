528e7d3cab8fc7ecf6aff82182688054249ba1ac
hanbinchoi <wkghskak@naver.com>
2023-09-10 21:23:06 +0900

    [FE] ✨ PostCard 컴포넌트 구현

--- client/src/app/board/page.tsx
+++ client/src/app/board/page.tsx
@@ -8,13 +8,14 @@ import BoardBanner from '@/components/board/BoardBanner';
 import BoardSearchForm from '@/components/board/BoardSearchForm';
 import RankBoard from '@/components/board/RankBoard';
 import CommonButton from '@/components/common/CommonButton';
+import PostCard from '@/components/board/PostCard';
 
 export default function Board() {
   const router = useRouter();
 
   const userId = useTestUserStore((state) => state.userId);
 
-  const navigaeToAddPost = () => {
+  const navigateToAddPost = () => {
     if (userId !== null) return router.push('/post/add');
     return router.push('/signin');
   };
@@ -24,14 +25,23 @@ export default function Board() {
       <RankBoard />
       <BoardBanner />
       <div className="relative w-full max-w-[720px] h-[487px] border-gradient">
-        <div className="p-4 flex flex-col gap-5">
+        <div className="p-4 flex flex-col gap-6">
           <div className="flex justify-between">
             <BoardSearchForm />
-            <CommonButton type="button" size="sm" onClick={navigaeToAddPost}>
+            <CommonButton type="button" size="sm" onClick={navigateToAddPost}>
               글 쓰기
             </CommonButton>
           </div>
-          <div className="pr-3 w-full h-[404px] flex flex-wrap  gap-4 overflow-y-scroll scrollbar"></div>
+          <div className="pr-3 w-full h-[404px] flex flex-wrap  gap-4 overflow-y-scroll scrollbar">
+            <div className="flex flex-wrap gap-4">
+              <PostCard
+                title={'첫 바질!!'}
+                imageSrc={'/assets/img/profile_avocado.png'}
+                likesNum={200}
+                commentsNum={21}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ client/src/components/board/PostCard.tsx
@@ -0,0 +1,52 @@
+import Image from 'next/image';
+
+interface PostCardProps {
+  title: string;
+  imageSrc: string;
+  likesNum: number;
+  commentsNum: number;
+}
+
+export default function PostCard({
+  title,
+  imageSrc,
+  likesNum,
+  commentsNum,
+}: PostCardProps) {
+  return (
+    <div className="w-[200px] h-[175px] pt-[0.875rem] px-[0.625rem] pb-[0.625rem] flex flex-col bg-brown-10 border-2 border-brown-50 rounded-lg">
+      <div className="w-[121px] h-[92px] rounded-lg overflow-hidden mb-2 mx-auto">
+        <Image
+          className="object-cover w-full h-full"
+          src={imageSrc}
+          alt=""
+          width={131}
+          height={102}
+        />
+      </div>
+      <h2 className="text-center text-sm leading-[0.875rem] text-brown-80 font-bold mb-[17px]">
+        {title}
+      </h2>
+      <div className="flex gap-3">
+        <span className="flex gap-[0.375rem]">
+          <Image
+            src="/assets/img/like.svg"
+            alt="좋아요 개수"
+            width={19}
+            height={16}
+          />
+          {likesNum}
+        </span>
+        <span className="flex gap-[0.375rem] text-base leading-4 text-brown-70">
+          <Image
+            src="/assets/img/comment.svg"
+            alt="좋아요 개수"
+            width={19}
+            height={16}
+          />
+          {commentsNum}
+        </span>
+      </div>
+    </div>
+  );
+}
