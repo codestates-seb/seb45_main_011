a72d9a1da9eb5e4e2f6d459bcd749d7b5784f085
hanbinchoi <wkghskak@naver.com>
2023-09-10 20:20:11 +0900

    [FE] ✨ BoardBanner 컴포넌트 구현

--- client/src/app/board/page.tsx
+++ client/src/app/board/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import BoardBanner from '@/components/board/BoardBanner';
 import RankBoard from '@/components/board/RankBoard';
 
 export default function Board() {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col gap-5 justify-center items-center">
       <RankBoard />
+      <BoardBanner />
       <div className="relative w-full max-w-[720px] h-[487px] border-gradient">
         <div className="pt-5 pb-4 pl-6 pr-5 flex flex-col gap-5"></div>
       </div>
