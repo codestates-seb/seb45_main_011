529327954583674aa6cf21a8829d9f02f6afc087
hanbinchoi <wkghskak@naver.com>
2023-09-12 01:30:32 +0900

    [FE] ✨ PostContent 컴포넌트 구현

--- client/src/app/post/[id]/page.tsx
+++ client/src/app/post/[id]/page.tsx
@@ -1,6 +1,8 @@
 import PageTitle from '@/components/common/PageTitle';
 import Screws from '@/components/common/Screws';
+import PostContent from '@/components/post/PostContent';
 import DateAndControl from '@/components/post/PostDateAndControl';
+import PostImage from '@/components/post/PostImage';
 import BoardProfile from '@/components/post/PostProfile';
 
 interface PostProps {
@@ -11,12 +13,12 @@ export default function Post({ params }: PostProps) {
   const boardId = Number(params.id);
   return (
     <main className="flex justify-center items-center">
-      <div className="relative w-full max-w-[720px] h-[645px] border-gradient rounded-xl">
-        <Screws />
-        <div className="h-full py-5">
-          <div className="pr-5 pl-7 h-full flex flex-col items-center overflow-y-scroll scrollbar">
+      <div className="relative w-full max-w-[720px] h-[864px] border-gradient rounded-xl">
+        <div className="h-full px-5 py-5">
+          <Screws />
+          <div className="relative h-full pr-5 pl-7 flex flex-col items-center overflow-y-scroll scrollbar ">
             <PageTitle text="첫 바질!!!" />
-            <div className="w-full flex justify-between items-center">
+            <div className="w-full flex justify-between items-center mb-4">
               <BoardProfile
                 displayName="관리자"
                 userId={1}
@@ -28,6 +30,10 @@ export default function Post({ params }: PostProps) {
                 usage="post"
                 boardId={boardId}
               />
+            </div>
+            <div className="px-[1.875rem] py-[1.625rem] w-full bg-brown-10 border-2 border-brown-50 rounded-lg mb-8">
+              <PostImage src={'/assets/img/profile_hitmontop.png'} />
+              <PostContent content={`asda`} />
             </div>
           </div>
         </div>
--- client/src/components/post/PostDateAndControl.tsx
+++ client/src/components/post/PostDateAndControl.tsx
@@ -12,7 +12,7 @@ export default function DateAndControl({
   boardId,
 }: DateAndControlProps) {
   return (
-    <div className="flex items-center gap-2 absolute z-10 right-[28px]">
+    <div className="flex items-center gap-2 absolute z-10 right-[36px]">
       <span className="text-sm leading-[0.875rem] font-bold text-brown-80">
         {date}
       </span>
