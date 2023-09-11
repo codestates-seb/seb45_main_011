fe2f0487b30fe030393835fca7bd084e0985a7bf
hanbinchoi <wkghskak@naver.com>
2023-09-11 20:03:08 +0900

    [FE] ✨ 부모 컨테이너 반응형 추가

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -5,11 +5,11 @@ import Image from 'next/image';
 export default function NotFound() {
   return (
     <main className="w-screen h-screen flex justify-center items-center bg-cover bg-center bg-no-repeat bg-[url('/assets/img/bg_default.png')]">
-      <div className="relative w-full max-w-[480px] min-w-[328px] border-gradient rounded-xl">
+      <div className="relative w-full max-w-[480px] min-w-[328px] border-gradient rounded-xl max-[360px]:w-[328px]">
         <div className="h-full pt-10 pb-11 flex flex-col items-center gap-6">
           <Screws />
           <Logo size="medium" className="mb-[24px]" />
-          <div className="w-full max-w-[320px] pt-10 pb-14 flex flex-col items-center gap-3 bg-[url('/assets/img/bg_board_md.png')] bg-center drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
+          <div className="w-full max-w-[320px] pt-10 pb-14 flex flex-col items-center gap-3 bg-[url('/assets/img/bg_board_md.png')] bg-center drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] ">
             <Image
               className="w-[60px] h-[60px]"
               src="/assets/img/warning.svg"
