93220da15173ff24272c92e8c2e557583cbfe1bc
nalsae <nalsae0207@naver.com>
2023-09-11 21:49:43 +0900

    [FE] ♻️ Refactor : Garden 페이지 관련 불필요한 로직 제거 (#153)

--- client/src/app/garden/[id]/layout.tsx
+++ client/src/app/garden/[id]/layout.tsx
@@ -1,16 +1,6 @@
-
 'use client';
 
-import { QueryClientProvider } from '@tanstack/react-query';
-
 import Header from '@/components/common/header/Header';
-
-
-import client from '@/api/client';
-
-=======
-import Header from '@/components/common/Header/Header';
-
 
 export default function GardenLayout({
   children,
@@ -18,16 +8,11 @@ export default function GardenLayout({
   children: React.ReactNode;
 }) {
   return (
-
-<QueryClientProvider client={client}>
     <>
       <Header />
       <div className="flex flex-col items-center h-full bg-cover bg-center bg-no-repeat bg-[url('/assets/img/bg_default.png')]">
         {children}
       </div>
     </>
-    </QueryClientProvider>
-
-
   );
 }
