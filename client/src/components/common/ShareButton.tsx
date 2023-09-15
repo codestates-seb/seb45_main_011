1126d2d637e456da1b010a4cf8b537a895192c5a
hanbinchoi <wkghskak@naver.com>
2023-09-15 13:10:07 +0900

    [FE] ✨ 공유하기 (leaf, leafs) 구현

--- client/src/components/common/ShareButton.tsx
+++ client/src/components/common/ShareButton.tsx
@@ -3,12 +3,14 @@ import { usePathname } from 'next/navigation';
 
 import useLeafsStore from '@/stores/leafsStore';
 import useLeafStore from '@/stores/leafStore';
+import CommonButton from './CommonButton';
 
 interface ShareButtonProps {
   location: 'leafs' | 'leaf';
+  position: 'top' | 'bottom';
 }
 
-export default function ShareButton({ location }: ShareButtonProps) {
+export default function ShareButton({ location, position }: ShareButtonProps) {
   const url = usePathname();
 
   const { modalOpen: leafsModalOpen, setModalCategory: setLeafsModalCategory } =
@@ -34,17 +36,28 @@ export default function ShareButton({ location }: ShareButtonProps) {
     }
   };
 
-  return (
-    <button
-      className="absolute top-[36px] right-[36px] w-[40px] h-[40px] bg-brown-50 rounded-[50%] border-2 border-brown-70 shadow-outer/down hover:scale-110 transition-transform"
-      onClick={shareUrl}>
-      <Image
-        className="relative left-[10px]"
-        src="/assets/icon/share.svg"
-        width={16}
-        height={18}
-        alt="공유하기"
-      />
-    </button>
-  );
+  if (position === 'top')
+    return (
+      <button
+        className="absolute top-[36px] right-[36px] w-[40px] h-[40px] bg-brown-50 rounded-[50%] border-2 border-brown-70 shadow-outer/down hover:scale-110 transition-transform  max-[550px]:hidden"
+        onClick={shareUrl}>
+        <Image
+          className="relative left-[10px]"
+          src="/assets/icon/share.svg"
+          width={16}
+          height={18}
+          alt="공유하기"
+        />
+      </button>
+    );
+  if (position === 'bottom')
+    return (
+      <CommonButton
+        size="md"
+        type="button"
+        onClick={shareUrl}
+        className="hidden w-fit hover:scale-105 hover:transition-transform max-[550px]:inline">
+        공유하기
+      </CommonButton>
+    );
 }
