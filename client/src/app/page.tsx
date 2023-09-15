2568021ed65072fe8e2f7f4f23510471abec2943
nalsae <nalsae0207@naver.com>
2023-09-16 05:50:26 +0900

    [FE] ✨ Feat : Main 페이지 임시 구현 (#238)

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,24 +1,76 @@
-import Header from '@/components/common/Header';
-import Intro from '@/components/common/Intro';
+'use client';
 
-import { INTRO_DESC } from '@/constants/contents';
+import { motion } from 'framer-motion';
+
+import useClient from '@/hooks/useClient';
+import useMainPageButtonAnimation from '@/hooks/useMainPageButtonAnimation';
+
+import Header from '@/components/common/Header';
+import LoadingNotice from '@/components/common/LoadingNotice';
+import Intro from '@/components/common/Intro';
+import ServiceInfo from '@/components/main/ServiceInfo';
+
+import { getScrollTop } from '@/utils/getScrollTop';
+
+import { MOUNT_ANIMATION_VALUES } from '@/constants/values';
 
 export default function Home() {
+  const isClient = useClient();
+  const { section, button } = useMainPageButtonAnimation(isClient);
+
+  const handleClick = () => {
+    const top = getScrollTop(window.innerWidth);
+
+    scroll({ top, behavior: 'smooth' });
+  };
+
   return (
     <>
       <Header />
-      <div className="h-full bg-cover bg-center bg-no-repeat bg-[url('/assets/img/bg_default.png')]">
-        <main className="flex flex-col items-center w-fit mx-auto">
-          <Intro className="mt-[120px]">
-            <div className="py-[10px] px-5 border-[3px] mb-4 border-brown-70 rounded-xl bg-contain bg-repeat bg-[url('/assets/img/bg_wood_dark.png')] text-xl text-brown-10 font-bold shadow-outer/down">
-              <p>{INTRO_DESC.first}</p>
-              <p className="leading-9">
-                <b className="text-brown-20 text-2xl">{INTRO_DESC.bold}</b>
-                {INTRO_DESC.second}
-              </p>
+      <div className="relative h-full bg-cover bg-center bg-no-repeat bg-[url('/assets/img/bg_default.png')]">
+        {!isClient ? (
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center">
+            <LoadingNotice isTransparent={false} />
+          </div>
+        ) : (
+          <>
+            <main
+              className={`flex flex-col items-center w-fit mx-auto ${
+                isClient ? 'block' : 'hidden'
+              }`}>
+              <motion.div
+                variants={MOUNT_ANIMATION_VALUES}
+                initial="initial"
+                animate="animate">
+                <Intro />
+              </motion.div>
+              <div
+                onClick={handleClick}
+                className="w-7 h-9 bg-red-30"
+                style={{
+                  marginTop: `${isClient && window.innerHeight / 7}px`,
+                  marginBottom: `${isClient && window.innerHeight / 7}px`,
+                }}
+              />
+            </main>
+            <div className="bg-gradient pb-[120px]">
+              <div className="flex flex-col items-center gap-[400px] pb-[200px] mt-[120px] mx-4">
+                {Array.from({ length: 3 }).map((_, index) => (
+                  <ServiceInfo key={index} order={index} />
+                ))}
+              </div>
+              <section
+                ref={section}
+                className="hover:scale-110 transition-transform">
+                <button
+                  ref={button}
+                  className="flex justify-center w-full max-w-[459px] h-fit py-8 mx-auto bg-blue-10">
+                  지금 바로 가입하기
+                </button>
+              </section>
             </div>
-          </Intro>
-        </main>
+          </>
+        )}
       </div>
     </>
   );
