1073307cae263b5f7774e5e9c11555b27e7a02d5
nalsae <nalsae0207@naver.com>
2023-09-15 13:12:17 +0900

    [FE] 💄 Style : 전체 페이지 스타일링 수정

--- client/src/app/post/add/page.tsx
+++ client/src/app/post/add/page.tsx
@@ -4,10 +4,12 @@ import PostForm from '@/components/post/PostForm';
 
 export default function AddPost() {
   return (
-    <div className="relative flex flex-col items-center min-w-[328px] max-w-[531px] mx-auto border-gradient rounded-xl bg-repeat shadow-container max-[563px]:mx-4">
-      <PageTitle text="게시글 등록" className="mt-5 mb-7" />
-      <PostForm mode="add" />
-      <Screws />
+    <div className="pb-[60px]">
+      <div className="relative flex flex-col items-center min-w-[328px] max-w-[531px] mx-auto border-gradient rounded-xl bg-repeat shadow-container max-[563px]:mx-4">
+        <PageTitle text="게시글 등록" className="mt-5 mb-7" />
+        <PostForm mode="add" />
+        <Screws />
+      </div>
     </div>
   );
 }
--- client/src/app/post/edit/[id]/page.tsx
+++ client/src/app/post/edit/[id]/page.tsx
@@ -26,19 +26,21 @@ export default function EditPost({ params }: EditPostProps) {
   if (isError) return <ErrorNotice isTransparent={false} className="mx-auto" />;
 
   return (
-    <div className="relative flex flex-col items-center min-w-[328px] max-w-[531px] mx-auto border-gradient rounded-xl bg-repeat shadow-container max-[563px]:mx-4">
-      {isLoading ? (
-        <LoadingNotice
-          isTransparent={true}
-          className="flex items-center h-[704px]"
-        />
-      ) : (
-        <>
-          <PageTitle text="게시글 수정" className="mt-5 mb-7" />
-          <PostForm post={post} postId={params.id} mode="edit" />
-        </>
-      )}
-      <Screws />
+    <div className="pb-[60px]">
+      <div className="relative flex flex-col items-center min-w-[328px] max-w-[531px] mx-auto border-gradient rounded-xl bg-repeat shadow-container max-[563px]:mx-4">
+        {isLoading ? (
+          <LoadingNotice
+            isTransparent={true}
+            className="flex items-center h-[704px]"
+          />
+        ) : (
+          <>
+            <PageTitle text="게시글 수정" className="mt-5 mb-7" />
+            <PostForm post={post} postId={params.id} mode="edit" />
+          </>
+        )}
+        <Screws />
+      </div>
     </div>
   );
 }
