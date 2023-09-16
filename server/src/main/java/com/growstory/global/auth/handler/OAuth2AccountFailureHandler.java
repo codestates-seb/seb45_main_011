34eb00da86285daac3f81936a3fd631beacfb36a
dohyoungK <gdh012@pusan.ac.kr>
2023-09-16 22:55:13 +0900

    [BE] ♻️ Refactor : Security 리팩토링

---
+++ server/src/main/java/com/growstory/global/auth/handler/OAuth2AccountFailureHandler.java
@@ -0,0 +1,19 @@
+package com.growstory.global.auth.handler;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class OAuth2AccountFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        if (exception instanceof Exception) {
+            System.out.println(exception.getCause());
+            response.sendRedirect("http://localhost");
+        }
+    }
+}
