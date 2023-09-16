37c956330c9b66365aaaf45d7c3ad81d5f973d37
nalsae <nalsae0207@naver.com>
2023-09-16 20:21:20 +0900

    [FE] 💄 Style : Loading, Error 컴포넌트 애니메이션 (#241)

--- client/src/components/common/ErrorNotice.tsx
+++ client/src/components/common/ErrorNotice.tsx
@@ -1,9 +1,14 @@
+'use client';
+
 import { twMerge } from 'tailwind-merge';
+import { motion } from 'framer-motion';
 
 import ErrorMessage from './ErrorMessage';
 import Screws from './Screws';
 
 import { DefaultProps } from '@/types/common';
+
+import { MOUNT_ANIMATION_VALUES } from '@/constants/values';
 
 interface ErrorNoticeProps extends DefaultProps {
   isTransparent: boolean;
@@ -16,18 +21,25 @@ export default function ErrorNotice({
   return (
     <>
       {isTransparent ? (
-        <div className={className}>
+        <motion.div
+          variants={MOUNT_ANIMATION_VALUES}
+          initial="initial"
+          animate="animate"
+          className={className}>
           <ErrorMessage />
-        </div>
+        </motion.div>
       ) : (
-        <div
+        <motion.div
+          variants={MOUNT_ANIMATION_VALUES}
+          initial="initial"
+          animate="animate"
           className={twMerge(
             'relative flex justify-center items-center w-[328px] h-[240px] border-gradient rounded-xl bg-repeat shadow-outer/down',
             className,
           )}>
           <ErrorMessage />
           <Screws />
-        </div>
+        </motion.div>
       )}
     </>
   );
