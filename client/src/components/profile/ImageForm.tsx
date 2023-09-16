a892d4b8813e8b2f6ccb7ceab601f417c1ed3a8f
nalsae <nalsae0207@naver.com>
2023-09-16 20:17:41 +0900

    [FE] 🐛 Fix : ImageForm 컴포넌트 관련 경고 해결 (#241)

--- client/src/components/profile/ImageForm.tsx
+++ client/src/components/profile/ImageForm.tsx
@@ -72,6 +72,7 @@ export default function ImageForm({ token, className }: ImageFormProps) {
             alt="profile_img"
             width={100}
             height={100}
+            priority
             onClick={() => imageUploadRef.current?.click()}
           />
           <input
