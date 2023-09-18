889a05d8f08f1e5a30ccde7719cc224faeda8fa8
nalsae <nalsae0207@naver.com>
2023-09-18 18:25:33 +0900

    [FE] 🐛 Fix : 404 페이지 누락된 애니메이션 구현 (#259)

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,14 +1,27 @@
+'use client';
+
+import Image from 'next/image';
+
+import { motion } from 'framer-motion';
+
 import Logo from '@/components/common/Logo';
 import Screws from '@/components/common/Screws';
-import Image from 'next/image';
+
+import { MOUNT_ANIMATION_VALUES } from '@/constants/values';
 
 export default function NotFound() {
   return (
-    <main className="w-screen h-screen flex justify-center items-center bg-cover bg-center bg-no-repeat bg-[url('/assets/img/bg_default.png')]">
-      <div className="relative w-full max-w-[480px] min-w-[328px] border-gradient rounded-xl max-[360px]:w-[328px]">
-        <div className="pt-10 pb-11 flex flex-col items-center gap-6 ">
+    <main className="w-screen h-screen pb-10 flex justify-center items-center bg-cover bg-center bg-no-repeat bg-[url('/assets/img/bg_default.png')]">
+      <motion.div
+        variants={MOUNT_ANIMATION_VALUES}
+        initial="initial"
+        animate="animate"
+        className="relative w-full max-w-[480px] min-w-[328px] border-gradient rounded-xl max-[360px]:w-[328px] shadow-container">
+        <div className="pt-10 pb-11 flex flex-col items-center gap-6">
           <Screws />
-          <Logo size="medium" className="mb-[24px]" />
+          <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+            <Logo size="medium" className="mb-[24px]" />
+          </motion.div>
           <div className="w-full max-w-[320px] pt-10 pb-14 flex flex-col items-center gap-3 bg-[url('/assets/img/bg_board_md.png')] bg-center bg-no-repeat bg-cover drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] max-[360px]:w-[280px] max-[360px]:pt-11 max-[360px]:pb-10 ">
             <Image
               className="w-[60px] h-[60px] max-[360px]:w-12 max-[360px]:h-12"
@@ -25,7 +38,7 @@ export default function NotFound() {
             </p>
           </div>
         </div>
-      </div>
+      </motion.div>
     </main>
   );
 }
