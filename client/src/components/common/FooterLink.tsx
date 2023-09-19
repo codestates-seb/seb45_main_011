e5fc0f8dbb5d0e0064303c894cd7349653a59560
hanbinchoi <wkghskak@naver.com>
2023-09-19 15:21:46 +0900

    [FE] 🐛 footer 링크 새 탭으로 열기

--- client/src/components/common/FooterLink.tsx
+++ client/src/components/common/FooterLink.tsx
@@ -19,7 +19,9 @@ export default function FooterLink({ repositories, member }: FooterLinkProps) {
   return (
     <Link
       href={FOOTER_LINK[repositories]}
-      className="hover:text-yellow-50 hover:scale-110 transition-all">
+      className="hover:text-yellow-50 hover:scale-110 transition-all"
+      target="_blank"
+      rel="noreferrer noopener">
       {member}
     </Link>
   );
