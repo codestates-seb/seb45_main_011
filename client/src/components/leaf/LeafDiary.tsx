f4813cbe3c4f4e681e62cdae4936bbd99ecdb9ec
hanbinchoi <wkghskak@naver.com>
2023-09-19 17:59:58 +0900

    [FE] ♻️ 다이어리 스크롤 위치 변경

--- client/src/components/leaf/LeafDiary.tsx
+++ client/src/components/leaf/LeafDiary.tsx
@@ -9,7 +9,7 @@ interface LeafDiaryProps {
 
 export default function LeafDiary({ diaries, pathUserId }: LeafDiaryProps) {
   return (
-    <ul className="w-full flex flex-col items-center gap-4 ">
+    <ul className="w-full h-[200px] flex flex-col items-center gap-4 overflow-y-scroll scrollbar pr-2 pb-4 ">
       {diaries.map((diary) => (
         <Diary
           key={diary.journalId}
