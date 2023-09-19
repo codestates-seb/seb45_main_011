da1e14679ed921ab3f530af9d78d1dbfb34b10d0
hanbinchoi <wkghskak@naver.com>
2023-09-19 18:00:29 +0900

    [FE] 🐛 다른 유저 식물카드 이름 조회 버그 수정

--- client/src/app/leafs/[id]/page.tsx
+++ client/src/app/leafs/[id]/page.tsx
@@ -6,6 +6,7 @@ import { useQuery } from '@tanstack/react-query';
 import { motion } from 'framer-motion';
 
 import { getLeafsByUserId } from '@/api/leaf';
+import { getUserInfo } from '@/api/history';
 
 import useLeafsStore from '@/stores/leafsStore';
 import useUserStore from '@/stores/userStore';
@@ -55,6 +56,11 @@ export default function Leafs({ params }: LeafsProps) {
     enabled: !!userId,
   });
 
+  const { data: user } = useQuery({
+    queryKey: ['user', pathUserId],
+    queryFn: () => getUserInfo(pathUserId),
+  });
+
   return (
     <>
       <motion.div
@@ -76,13 +82,13 @@ export default function Leafs({ params }: LeafsProps) {
               <ErrorMessage />
             </div>
           )}
-          {leafs && (
+          {leafs && user && (
             <div className="pt-7 pb-4 pl-6 pr-5 flex flex-col gap-5">
               <PageTitle
                 text={
                   pathUserId === userId
                     ? '내 식물 카드'
-                    : `${leafs[0].displayName} 님의 식물 카드`
+                    : `${user.data.displayName} 님의 식물 카드`
                 }
               />
               <div className="pt-2 pb-2 pl-2 pr-4 w-full h-[404px] overflow-y-scroll scrollbar grid grid-cols-3 gap-4 place-items-center items-start max-[730px]:grid-cols-2 max-[530px]:grid-cols-1">
