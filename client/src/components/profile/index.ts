809b2f69fcc5ae4875ee819ac337dc25139d57a4
심도연 <shimdokite@gmail.com>
2023-10-10 22:55:03 +0900

    [Refactor] ♻️ signin. signup import 문 병합

---
+++ client/src/components/profile/index.ts
@@ -0,0 +1,15 @@
+import ChangeNicknameModal from './ChangeNicknameModal';
+import ChangePasswordModal from './ChangePasswordModal';
+import ImageForm from './ImageForm';
+import NicknameForm from './NicknameForm';
+import PasswordForm from './PasswordForm';
+import ProfileBox from './ProfileBox';
+
+export {
+  ChangeNicknameModal,
+  ChangePasswordModal,
+  ImageForm,
+  NicknameForm,
+  PasswordForm,
+  ProfileBox,
+};
---
+++ client/src/components/signup/index.ts
@@ -0,0 +1,7 @@
+import AuthEmailModal from './AuthEmailModal';
+import FailureModal from './FailureModal';
+import SignButton from './SignButton';
+import SignupForm from './SignupForm';
+import SignupIntro from './SignupIntro';
+
+export { AuthEmailModal, FailureModal, SignButton, SignupForm, SignupIntro };
