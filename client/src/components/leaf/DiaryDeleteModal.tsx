94491898c81143b65f741e5f241493212f411f97
hanbinchoi <wkghskak@naver.com>
2023-10-14 02:45:34 +0900

    [FE] ♻️ DiaryDeleteModal 컴포넌트 리팩토링

--- client/src/components/leaf/DiaryDeleteModal.tsx
+++ client/src/components/leaf/DiaryDeleteModal.tsx
@@ -1,7 +1,10 @@
 import useLeafStore from '@/stores/leafStore';
 
-import CommonButton from '../common/CommonButton';
 import useDeleteDiaryMutation from '@/hooks/useDeleteDiaryMutaion';
+
+import { CommonButton } from '../common';
+
+import { DIARY_DELETE_MODAL_TEXT } from '@/constants/contents';
 
 interface DiaryDeleteModalProps {
   deleteTargetId?: string | null;
@@ -9,16 +12,16 @@ interface DiaryDeleteModalProps {
   userId: string;
 }
 
-export function DiaryDeleteModal({
+export default function DiaryDeleteModal({
   userId,
   leafId,
   deleteTargetId,
 }: DiaryDeleteModalProps) {
-  if (!deleteTargetId) return null;
+  const modalClose = useLeafStore((state) => state.modalClose);
 
   const { mutate: deleteDiary } = useDeleteDiaryMutation(leafId);
 
-  const modalClose = useLeafStore((state) => state.modalClose);
+  if (!deleteTargetId) return null;
 
   const handleCancelModal = () => modalClose();
 
@@ -30,11 +33,12 @@ export function DiaryDeleteModal({
   return (
     <div className="w-[320px] px-[2rem] py-[1.5rem] flex flex-col justify-center">
       <p className="text-center font-bold text-[1.6rem] leading-8 text-brown-70 mb-1 break-words">
-        선택한 일지를
+        {DIARY_DELETE_MODAL_TEXT.firstLine}
       </p>
       <p className="text-center font-bold text-[1.6rem] leading-8 text-brown-90 mb-4 break-words">
         {/* 그래도 <br className="hidden max-[400px]:inline" /> */}
-        <b className="text-red-50">삭제</b>하시겠습니까?
+        <b className="text-red-50">{DIARY_DELETE_MODAL_TEXT.secondLine[0]}</b>
+        {DIARY_DELETE_MODAL_TEXT.secondLine[1]}
       </p>
       <div className="flex gap-3 justify-center">
         <CommonButton
@@ -42,14 +46,14 @@ export function DiaryDeleteModal({
           size="md"
           onClick={handleDeleteDiary}
           className="hover:scale-105 hover:transition-transform">
-          삭제
+          {DIARY_DELETE_MODAL_TEXT.button[0]}
         </CommonButton>
         <CommonButton
           type="button"
           size="md"
           onClick={handleCancelModal}
           className="hover:scale-105 hover:transition-transform">
-          취소
+          {DIARY_DELETE_MODAL_TEXT.button[1]}
         </CommonButton>
       </div>
     </div>
