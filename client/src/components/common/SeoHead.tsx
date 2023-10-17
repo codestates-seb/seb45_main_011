3399039d5471a257ff7cbf6ba80d8c534efe1c91
hanbinchoi <wkghskak@naver.com>
2023-10-17 16:47:13 +0900

    [FE] ♻️ SeoHead 컴포넌트 주석 처리

--- client/src/components/common/SeoHead.tsx
+++ client/src/components/common/SeoHead.tsx
@@ -11,10 +11,10 @@ import { PageType, Post } from '@/types/common';
 //   page: PageType;
 // };
 
-export default function SeoHead({ post, page }: MetaDataProps) {
-  const router = useRouter();
-  // const metaData = getMetaData(page, router.query, post);
-  //TODO: 주석에 원하는대로 값이 나오는지 테스트가 필요하다
-  //TODO: og 이미지는 어떻게 할 것인가?
-  return;
-}
+// export default function SeoHead({ post, page }: MetaDataProps) {
+//   const router = useRouter();
+// const metaData = getMetaData(page, router.query, post);
+//TODO: 주석에 원하는대로 값이 나오는지 테스트가 필요하다
+//TODO: og 이미지는 어떻게 할 것인가?
+//   return;
+// }
