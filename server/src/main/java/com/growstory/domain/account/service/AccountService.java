0a47cd45a0b9709adf8bd7bb247907699b0b0040
LST <dev.st1027@gmail.com>
2023-11-09 16:22:57 +0900

    :bug: Fix: 비속어 예외처리 NPE 로직 개선4

--- server/src/main/java/com/growstory/domain/account/service/AccountService.java
+++ server/src/main/java/com/growstory/domain/account/service/AccountService.java
@@ -226,6 +226,7 @@ public class AccountService {
         if (Long.valueOf((String) claims.get("accountId")) != accountId) {
             log.info("##" + claims.get("accountId"));
             log.info("##" + accountId);
+            log.info("##" + (Long.valueOf((String) claims.get("accountId"))!=accountId));
             throw new BusinessLogicException(ExceptionCode.ACCOUNT_NOT_ALLOW);
         }
     }
