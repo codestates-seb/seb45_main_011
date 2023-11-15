ec3af58f033601ef9ad890dd5f975236b308124b
LST <dev.st1027@gmail.com>
2023-11-15 15:27:13 +0900

    :sparkles: Feat: ChatRoom, ChatMessage 상수 작성

---
+++ server/src/main/java/com/growstory/domain/qnachat/chatroom/constants/ChatRoomConstants.java
@@ -0,0 +1,16 @@
+package com.growstory.domain.qnachat.chatroom.constants;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class ChatRoomConstants {
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public enum EnumChatRoomMessage {
+        enumExitChatRoomMessage("님이 채팅방을 퇴장하셨습니다.");
+        private String value;
+    }
+}
