67cc2230c9d4d72753e6303f8db02bd7abd9dd53
LST <dev.st1027@gmail.com>
2023-11-15 14:55:06 +0900

    :wrench: Config: S3 버킷 리전 이슈 해결

--- server/src/main/java/com/growstory/global/aws/config/S3Config.java
+++ server/src/main/java/com/growstory/global/aws/config/S3Config.java
@@ -3,7 +3,7 @@ package com.growstory.global.aws.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -21,10 +21,10 @@ public class S3Config {
     private String region;
 
     @Bean
-    public AmazonS3 amazonS3Client() {
+    public AmazonS3Client amazonS3Client() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
-        return AmazonS3ClientBuilder
+        return (AmazonS3Client) AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(region)
