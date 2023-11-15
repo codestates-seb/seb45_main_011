335e1643dd95e565539e784893fb4c37c3587c31
LST <dev.st1027@gmail.com>
2023-11-15 15:27:59 +0900

    :sparkles: Feat: AccountChatRoom, ChatMessage, ChatRoom Repository 작성

---
+++ server/src/main/java/com/growstory/domain/images/repository/ChatMessageImageRepository.java
@@ -0,0 +1,7 @@
+package com.growstory.domain.images.repository;
+
+import com.growstory.domain.images.entity.ChatMessageImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatMessageImageRepository extends JpaRepository<ChatMessageImage, Long> {
+}
---
+++ server/src/main/java/com/growstory/domain/qnachat/chatmessage/repository/ChatMessageRepository.java
@@ -0,0 +1,26 @@
+package com.growstory.domain.qnachat.chatmessage.repository;
+
+import com.growstory.domain.qnachat.chatmessage.entity.ChatMessage;
+import com.growstory.domain.qnachat.chatroom.entity.ChatRoom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    //채팅방 페이지 조회
+    Page<ChatMessage> findByChatRoom(Pageable pageable, ChatRoom chatRoom);
+
+
+    //채팅방 조건 메시지 조회
+    List<ChatMessage> findAllByChatRoom(ChatRoom chatRoom);
+
+    List<ChatMessage> findAllByChatRoom(ChatRoom chatRoom, Sort sort);
+
+    //메시지 내용 조건 메시지 조회
+    List<ChatMessage> findAllByMessageContaining(String message);
+
+    List<ChatMessage> findAllByMessageContaining(String message, Sort sort);
+}
