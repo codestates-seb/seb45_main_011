425210a0e3eb304286e929e031f6e634f2a5c50f
LST <dev.st1027@gmail.com>
2023-12-01 15:44:18 +0900

    :recycle: Refactor: 채팅방 조회 생성일과 최신 메시지 송신일을 yyyy-MM-dd 형식으로 초기화

--- server/src/main/java/com/growstory/domain/qnachat/chatroom/dto/ChatRoomResponseDto.java
+++ server/src/main/java/com/growstory/domain/qnachat/chatroom/dto/ChatRoomResponseDto.java
@@ -27,7 +27,7 @@ public class ChatRoomResponseDto {
     private String status;
     private Boolean isAnswered;
     private String latestMessage;
-    private LocalDateTime latestTime;
+    private String latestTime;
 
     public static ChatRoomResponseDto from(AccountChatRoom accountChatRoom, AccountChatRoom otherAccountChatRoom) {
 
@@ -46,10 +46,10 @@ public class ChatRoomResponseDto {
                 .otherAccountId(otherAccountChatRoom.getAccount().getAccountId())
                 .otherAccountName(otherAccountChatRoom.getAccount().getDisplayName())
                 .status(accountChatRoom.getChatRoom().getStatus().getMessage())
-                .createdAt(accountChatRoom.getChatRoom().getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                .createdAt(accountChatRoom.getChatRoom().getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
                 .isAnswered(accountChatRoom.getChatRoom().getIsAnswered())
                 .latestMessage(tempLatestMessage)
-                .latestTime(tempLatestTime)
+                .latestTime(tempLatestTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
                 .build();
     }
 
@@ -68,7 +68,7 @@ public class ChatRoomResponseDto {
         return ChatRoomResponseDto.builder()
                 .chatRoomId(accountChatRoom.getChatRoom().getChatRoomId())
                 .latestMessage(tempLatestMessage)
-                .latestTime(tempLatestTime)
+                .latestTime(tempLatestTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
                 .build();
 
     }
